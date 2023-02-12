f3f853cd2e51b6b7dfb9fb467d4972c01e2bb06f
ackintosh <sora.akatsuki@gmail.com>
2023-02-13 07:56:38 +0900

    aa

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -17,55 +17,55 @@ fn main() {
     let root_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("../../");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // Ensure libblst exists in `OUT_DIR`
-    // Assuming blst submodule exists
-    Command::new("make")
-        .current_dir(root_dir.join("src"))
-        .arg("blst")
-        .status()
-        .unwrap();
-    move_file(
-        root_dir.join("lib/libblst.a").as_path(),
-        out_dir.join("libblst.a").as_path(),
-    )
-    .unwrap();
-
-    let field_elements_per_blob = if cfg!(feature = "minimal-spec") {
-        MINIMAL_FIELD_ELEMENTS_PER_BLOB
-    } else {
-        MAINNET_FIELD_ELEMENTS_PER_BLOB
-    };
-
-    eprintln!("Using FIELD_ELEMENTS_PER_BLOB={}", field_elements_per_blob);
-
-    // Deleting any existing assembly and object files to ensure that compiling with a different
-    // feature flag changes the final linked library file.
-    let obj_file = root_dir.join("src/c_kzg_4844.o");
-    if obj_file.exists() {
-        std::fs::remove_file(obj_file).unwrap();
-    }
-
-    // Ensure libckzg exists in `OUT_DIR`
-    Command::new("make")
-        .current_dir(root_dir.join("src"))
-        .arg("all")
-        .arg(format!(
-            "FIELD_ELEMENTS_PER_BLOB={}",
-            field_elements_per_blob
-        ))
-        .status()
-        .unwrap();
-
-    Command::new("ar")
-        .current_dir(&root_dir.join("src"))
-        .args(["crus", "libckzg.a", "c_kzg_4844.o"])
-        .status()
-        .unwrap();
-    move_file(
-        root_dir.join("src/libckzg.a").as_path(),
-        out_dir.join("libckzg.a").as_path(),
-    )
-    .unwrap();
+    // // Ensure libblst exists in `OUT_DIR`
+    // // Assuming blst submodule exists
+    // Command::new("make")
+    //     .current_dir(root_dir.join("src"))
+    //     .arg("blst")
+    //     .status()
+    //     .unwrap();
+    // move_file(
+    //     root_dir.join("lib/libblst.a").as_path(),
+    //     out_dir.join("libblst.a").as_path(),
+    // )
+    // .unwrap();
+    //
+    // let field_elements_per_blob = if cfg!(feature = "minimal-spec") {
+    //     MINIMAL_FIELD_ELEMENTS_PER_BLOB
+    // } else {
+    //     MAINNET_FIELD_ELEMENTS_PER_BLOB
+    // };
+    //
+    // eprintln!("Using FIELD_ELEMENTS_PER_BLOB={}", field_elements_per_blob);
+    //
+    // // Deleting any existing assembly and object files to ensure that compiling with a different
+    // // feature flag changes the final linked library file.
+    // let obj_file = root_dir.join("src/c_kzg_4844.o");
+    // if obj_file.exists() {
+    //     std::fs::remove_file(obj_file).unwrap();
+    // }
+    //
+    // // Ensure libckzg exists in `OUT_DIR`
+    // Command::new("make")
+    //     .current_dir(root_dir.join("src"))
+    //     .arg("all")
+    //     .arg(format!(
+    //         "FIELD_ELEMENTS_PER_BLOB={}",
+    //         field_elements_per_blob
+    //     ))
+    //     .status()
+    //     .unwrap();
+    //
+    // Command::new("ar")
+    //     .current_dir(&root_dir.join("src"))
+    //     .args(["crus", "libckzg.a", "c_kzg_4844.o"])
+    //     .status()
+    //     .unwrap();
+    // move_file(
+    //     root_dir.join("src/libckzg.a").as_path(),
+    //     out_dir.join("libckzg.a").as_path(),
+    // )
+    // .unwrap();
 
     move_file(
         root_dir.join("blst/blst.lib").as_path(),
